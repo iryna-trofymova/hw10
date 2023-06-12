fc26896683531ca824949cf95fa050972706ecf0
Iryna Trofymova <trofimovairena92@gmail.com>
2023-06-12 14:28:54 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+/*Реалізуйте функцію generateKey(length, characters), що повертає рядок випадкових символів із набору characters довжиною length.
+
+Наприклад:
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const key = generateKey(16, characters);
+console.log(key); // eg599gb60q926j8i*/
+
+function generateKey(length, characters) {
+  let key = "";
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charactersLength);
+    key += characters.charAt(randomIndex);
+  }
+
+  return key;
+}
+
+const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+const key = generateKey(16, characters);
+console.log(key);
